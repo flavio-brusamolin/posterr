9e538612380ee3677d19a7ab295eb986770884f4
Flavio Brito <flavio.brito@gruposantander.com>
2022-04-13 20:40:59 -0300

    Add CreateRegularPostUseCase

---
+++ src/domain/use-cases/create-regular-post-use-case.ts
@@ -0,0 +1,9 @@
+import { RegularPost } from '../entities/regular-post'
+import { UseCase } from './use-case'
+
+export interface CreateRegularPostInput {
+  userId: string
+  content: string
+}
+
+export type CreateRegularPostUseCase = UseCase<CreateRegularPostInput, RegularPost>
