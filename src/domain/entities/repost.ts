002ce0af55d2c0ddecb7af5b88bd2ab9686b6da8
Flavio Brito <flavio.brito@gruposantander.com>
2022-04-13 20:36:28 -0300

    Add Repost entity

---
+++ src/domain/entities/repost.ts
@@ -0,0 +1,77 @@
+import { RegularPost } from './regular-post'
+import { PostType } from '../enums/post-type'
+
+interface RepostInput {
+  type?: PostType
+  postId: string
+  userId: string
+  createdAt?: Date
+  originalPost: RegularPost
+  comment?: string
+}
+
+export class Repost {
+  private type: PostType
+  private postId: string
+  private userId: string
+  private createdAt: Date
+  private originalPost: RegularPost
+  private comment?: string
+
+  constructor ({ type, postId, userId, createdAt, originalPost, comment }: RepostInput) {
+    this.setType(type)
+    this.setPostId(postId)
+    this.setUserId(userId)
+    this.setCreatedAt(createdAt)
+    this.setOriginalPost(originalPost)
+    this.setComment(comment)
+  }
+
+  private setType (type?: PostType): void {
+    this.type = type ?? PostType.REPOST
+  }
+
+  private setPostId (postId: string): void {
+    this.postId = postId
+  }
+
+  private setUserId (userId: string): void {
+    this.userId = userId
+  }
+
+  private setCreatedAt (createdAt?: Date): void {
+    this.createdAt = createdAt ?? new Date()
+  }
+
+  private setOriginalPost (originalPost: RegularPost): void {
+    this.originalPost = originalPost
+  }
+
+  private setComment (comment?: string): void {
+    this.comment = comment
+  }
+
+  getType (): PostType {
+    return this.type
+  }
+
+  getPostId (): string {
+    return this.postId
+  }
+
+  getUserId (): string {
+    return this.userId
+  }
+
+  getCreatedAt (): Date {
+    return this.createdAt
+  }
+
+  getOriginalPost (): RegularPost {
+    return this.originalPost
+  }
+
+  getComment (): string {
+    return this.comment
+  }
+}
