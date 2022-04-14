498c786785f4440de91f8224322c7c6009efb697
Flavio Brito <flavio.brito@gruposantander.com>
2022-04-14 19:32:27 -0300

    Add PostMongoRepository

---
+++ src/infrastructure/database/mongo/post/post-mongo-repository.ts
@@ -0,0 +1,50 @@
+import PostMongoSchema from './post-mongo-schema'
+import PostMongoMapper from './post-mongo-mapper'
+import { CountTodayPostsRepository } from '../../../../application/contracts/count-today-posts-repository'
+import { CreateRegularPostRepository } from '../../../../application/contracts/create-regular-post-repository'
+import { CreateRepostRepository } from '../../../../application/contracts/create-repost-repository'
+import { LoadPostRepository } from '../../../../application/contracts/load-post-repository'
+import { LoadPostsRepository } from '../../../../application/contracts/load-posts-repository'
+import { Post } from '../../../../domain/aggregates/post'
+import { RegularPost } from '../../../../domain/entities/regular-post'
+import { Repost } from '../../../../domain/entities/repost'
+
+export class PostMongoRepository implements CreateRegularPostRepository, CreateRepostRepository, CountTodayPostsRepository, LoadPostRepository, LoadPostsRepository {
+  async createRegularPost (regularPost: RegularPost): Promise<RegularPost> {
+    const regularPostRecord = await PostMongoSchema.create(regularPost)
+    const regularPostResponse = PostMongoMapper.toDomainEntity(regularPostRecord)
+    return (regularPostResponse instanceof RegularPost) && regularPostResponse
+  }
+
+  async createRepost (repost: Repost): Promise<Repost> {
+    const repostRecord = await PostMongoSchema.create(repost)
+    const repostResponse = PostMongoMapper.toDomainEntity(repostRecord)
+    return (repostResponse instanceof Repost) && repostResponse
+  }
+
+  async loadPost (postId: string): Promise<Post> {
+    const postRecord = await PostMongoSchema.findOne({ postId })
+    return postRecord && PostMongoMapper.toDomainEntity(postRecord)
+  }
+
+  async loadPosts (userIds?: string[]): Promise<Post[]> {
+    const query = userIds ? { userId: { $in: userIds } } : {}
+    const postRecords = await PostMongoSchema.find(query).sort('-createdAt')
+    return postRecords.map(PostMongoMapper.toDomainEntity)
+  }
+
+  async countTodayPosts (userId: string): Promise<number> {
+    const startOfDay = this.getStartOfDay()
+    const numberOfPosts = await PostMongoSchema.countDocuments({
+      userId,
+      createdAt: { $gte: startOfDay }
+    })
+    return numberOfPosts
+  }
+
+  private getStartOfDay (): Date {
+    const now = new Date()
+    const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+    return startOfDay
+  }
+}
