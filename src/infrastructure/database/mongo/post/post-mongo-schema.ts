575b5f23623b84113596e72327e037d68aebf1e8
Flavio Brito <flavio.brito@gruposantander.com>
2022-04-14 19:30:24 -0300

    Add PostMongoSchema

---
+++ src/infrastructure/database/mongo/post/post-mongo-schema.ts
@@ -0,0 +1,44 @@
+import { Schema, model } from 'mongoose'
+import { PostType } from '../../../../domain/enums/post-type'
+import { PostMongo } from './post-mongo-entity'
+
+const RegularPostSchema = {
+  type: {
+    type: String,
+    required: true,
+    enum: Object.values(PostType)
+  },
+  postId: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true
+  },
+  content: {
+    type: String,
+    required: false
+  }
+}
+
+const PostMongoSchema = new Schema({
+  ...RegularPostSchema,
+  originalPost: {
+    type: new Schema(RegularPostSchema, { _id: false }),
+    required: false
+  },
+  comment: {
+    type: String,
+    required: false
+  }
+})
+
+PostMongoSchema.index({ postId: 1 }, { unique: true })
+PostMongoSchema.index({ userId: 1 })
+
+export default model<PostMongo>('posts', PostMongoSchema)
