ebb6a0eede5389eca8ed3263b4c372bfecd4e757
Flavio Brito <flavio.brito@gruposantander.com>
2022-04-14 20:07:28 -0300

    Add follow user route

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeFollowUserController } from '../factories/controllers/follow-user-controller-factory'
 import { makeLoadUserController } from '../factories/controllers/load-user-controller-factory'
 
 export default (router: Router): void => {
+  router.post('/users/:userId/follow', adaptRoute(makeFollowUserController()))
   router.get('/users/:userId', adaptRoute(makeLoadUserController()))
 }
