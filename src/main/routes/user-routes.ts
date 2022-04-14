3d54a47c2f53dcf31e4e30b222c23e7db39aecd8
Flavio Brito <flavio.brito@gruposantander.com>
2022-04-14 20:16:58 -0300

    Add unfollow user route

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeFollowUserController } from '../factories/controllers/follow-user-controller-factory'
+import { makeUnfollowUserController } from '../factories/controllers/unfollow-user-controller-factory'
 import { makeLoadUserController } from '../factories/controllers/load-user-controller-factory'
 
 export default (router: Router): void => {
   router.post('/users/:userId/follow', adaptRoute(makeFollowUserController()))
+  router.post('/users/:userId/unfollow', adaptRoute(makeUnfollowUserController()))
   router.get('/users/:userId', adaptRoute(makeLoadUserController()))
 }
