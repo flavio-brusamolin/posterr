d4927f90e6385306d5f1e65735faaed41877b8ac
Flavio Brito <flavio.brito@gruposantander.com>
2022-04-14 19:41:31 -0300

    Add express route adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,17 @@
+import { RequestHandler } from 'express'
+import { Controller } from '../../interfaces/http/controllers/controller'
+import { HttpRequest } from '../../interfaces/http/contracts/http-request'
+
+export const adaptRoute = (controller: Controller): RequestHandler => {
+  return async (req, res) => {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+      headers: req.headers,
+      params: req.params,
+      query: req.query
+    }
+
+    const { statusCode, body } = await controller.handle(httpRequest)
+    res.status(statusCode).json(body)
+  }
+}
